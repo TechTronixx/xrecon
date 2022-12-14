110d64658204ecd7766c58f7d3b6e11bcf8dfaa5
ChiragChrg <chiruchirag2001@gmail.com>
2022-12-15 01:42:13 +0530

    Cors update

--- server/index.js
+++ server/index.js
@@ -13,11 +13,14 @@ const chatRoutes = require('./routes/chatRoutes');
 
 require('dotenv').config();
 app.use(express.json());
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: "*"
-}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['A']
+// }));
 
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
