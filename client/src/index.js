6c707baf98f9a8bd880189fbbad2291e10914815
ChiragChrg <chiruchirag2001@gmail.com>
2022-12-15 23:20:13 +0530

    removed addtl headers

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://192.168.0.102:5000/';
 axios.defaults.baseURL = 'https://xrecon.onrender.com/';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.common['Cache-Control'] = 'max-age=31536000';
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.common['Cache-Control'] = 'max-age=31536000';
 // axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
 // axios.defaults.headers.common['Content-Type'] = 'text/plain';
 
